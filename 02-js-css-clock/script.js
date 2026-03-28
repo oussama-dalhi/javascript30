961ab11be2cb3b4a6666f84f4ba3d6751558e1b9
oussama-dalhi <oussamadalhi00@gmail.com>
2026-03-28 01:37:21 +0100

    Added Digital clock display (HH:MM:SS)

--- 02-js-css-clock/script.js
+++ 02-js-css-clock/script.js
@@ -1,25 +1,33 @@
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
+const sec = document.querySelector(`.time span[data-list='second']`);
+const min = document.querySelector(`.time span[data-list='minute']`);
+const hour = document.querySelector(`.time span[data-list='hour']`);
+
 function setDate() {
     const now = new Date();
-    // seconds to degrees
     const seconds = now.getSeconds();
     const secondsToDeg = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsToDeg}deg)`;
-
     if (seconds === 0) {
      secondHand.style.transition = 'none';
     } else if (seconds === 1) {
      secondHand.style.transition = '';
     }
-    // minutes to degrees
+
     const minutes = now.getMinutes();
     const minutesToDeg = ((minutes +  seconds / 60) / 60 * 360) + 90;
     minuteHand.style.transform = `rotate(${minutesToDeg}deg)`;
-    // hours to degrees
-    const hour = now.getHours();
-    const hourToDeg = ((hour % 12 + minutes / 60) / 12) * 360 + 90;
+
+    const hours = now.getHours();
+    const hourToDeg = ((hours % 12 + minutes / 60) / 12) * 360 + 90;
     hourHand.style.transform = `rotate(${hourToDeg}deg)`;
+
+    const format = n => String(n).padStart(2, '0');
+    sec.textContent = format(seconds);
+    min.textContent = `${format(minutes)}:`;
+    hour.textContent = `${format(hours)}:`;
 }
+setDate();
 setInterval(setDate, 1000);
