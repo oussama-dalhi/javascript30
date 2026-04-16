a60258a0ce15cd533611d957f10af0eaebb72fc7
oussama-dalhi <oussamadalhi00@gmail.com>
2026-04-16 22:47:19 +0100

    Day 11: HTML5 Video Player

---
+++ 11-video-player/src/script.js
@@ -0,0 +1,53 @@
+const player = document.querySelector('.player');
+const video = document.querySelector('.viewer');
+const progress = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll('.player__slider');
+function togglePlay() {
+    video.paused ? video.play() : video.pause();
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+function skip() {
+    console.log(this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.clientWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(btn => btn.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => {
+    mousedown = true;
+});
+progress.addEventListener('mouseup', () => {
+    mousedown = false;
+});
